4cf569445e224989445efefcc186eeae1c64c115
Paige Stephenson <paigestephenson29@gmail.com>
2023-06-08 22:10:03 -0600

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Paige Stephenson on 6/8/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
