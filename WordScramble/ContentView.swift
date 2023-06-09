d5d40e74c735489db332f86a8393f644a96b6e05
Paige Stephenson <paigestephenson29@gmail.com>
2023-06-09 17:21:31 -0600

    Add WordScramble files

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -1,24 +1,132 @@
-//
+ //
 //  ContentView.swift
 //  WordScramble
 //
 //  Created by Paige Stephenson on 6/8/23.
 //
+//Three views: Navigation view, text field to enter in new words, scrolling list of previously used words
+//Add an array of words used already
+//Add a root word for them to spell from
+//A string combined to a text field
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                        .textInputAutocapitalization(.never)
+                    //      Bind this text field to a freeform text string - newWord
+                }
+                Section {
+                    ForEach(usedWords, id: \.self) { word in
+                        HStack {
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                            // id: \.self tells swift that every item in the usedWords array is unique
+                        }
+                    }
+                }
+            }
+            .navigationTitle(rootWord)
+            .onSubmit(addNewWord)
+            .onAppear(perform: startGame)
+            
+            .alert(errorTitle, isPresented: $showingError) {
+                Button("OK", role: .cancel) { }
+            } message: {
+                Text(errorMessage)
+            }
         }
-        .padding()
+    }
+    
+    func addNewWord() {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        guard answer.count > 0 else { return }
+        
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word used already", message: "Be more original")
+            return
+        }
+        
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not possible", message: "You can't spell that word from '\(rootWord)'!")
+            return
+        }
+        
+        guard isReal(word: answer) else {
+            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+            return
+        }
+
+        
+        withAnimation {
+            usedWords.insert(answer, at: 0)
+        }
+        newWord = ""
+    }
+    
+    func startGame() {
+//        1. Find the URL for start.txt in our app bundle
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+//            2. Load start.txt into a string
+            if let startWords = try? String(contentsOf: startWordsURL){
+//                3. Split the string into an array of strings, splitting on line breaks
+                let allWords = startWords.components(separatedBy: "\n")
+//                4. Pick one random word, or use "silkwordm" as a default
+                rootWord = allWords.randomElement() ?? "silkworm"
+                
+                return
+            }
+        }
+        fatalError("Could not load start.txt from bundle.")
+    }
+    
+//    Return true or false depending on whether the worde has been used before or not
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    
+//  Create a variable copy of the root wor, loop over each letter of the user's input word to see if that letter exists in our copy. If it does, remove it frmo the copy.
+    
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+        
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
